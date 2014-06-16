3ea95611f5b9a466a9353d0c9a24e234749a88d9
unknown <stdkl2@K3-C9EEE8663C85.UC.local>
2014-06-16 12:31:23 +0400

    Error messages

--- start.js
+++ start.js
@@ -2,10 +2,17 @@ var field = new Field();
 var fieldWrapper = document.getElementById("fieldWrapper");
 fieldWrapper.style.width = (20 + Math.min(800, level.width * CELL_SIZE.width)) + "px";
 fieldWrapper.style.height = (20 + Math.min(600, level.height * CELL_SIZE.height)) + "px";
-level.player.level = getCookie("level", 1);
+
+if(localStorage["level"] == undefined) {
+	localStorage["level"] = level.player.level = 1;
+} else {
+	level.player.level = +localStorage["level"];
+}
+
 var programDiv = document.getElementById("program");
 programDiv.style.width = fieldWrapper.style.width;
 programDiv.style.height = 20 + 64;
+
 loadAvailableCommands();
 loadDragAndDrop();
 initControls();
